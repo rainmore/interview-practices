114ff11f741a67616b787b902131253afd56b4a0
Felix Rong <frong@infomedia.com.au>
2024-03-15 14:14:01 +1100

    wip

---
+++ src/main/java/au/com/rainmore/graphs/No200NumberOfIslands.java
@@ -0,0 +1,39 @@
+package au.com.rainmore.graphs;
+
+
+/**
+ * <a href="https://leetcode.com/problems/number-of-islands/description/">
+ * 200. Number of Islands</a>
+ */
+public class No200NumberOfIslands {
+
+    public int numIslands(char[][] grid) {
+        int c = 0;
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid.length; j++) {
+                if (grid[i][j] == '1') {
+                    dfs(grid, i, j);
+                    c++;
+                }
+            }
+        }
+        return c;
+    }
+
+    private void dfs(char[][] grid, int i, int j) {
+        if (
+            i < 0 || j < 0 ||
+            i >= grid.length || j >= grid[0].length ||
+            grid[i][j] == '0'
+        ) {
+            return;
+        }
+
+        grid[i][j] = '0';
+        dfs(grid, i + 1, j);
+        dfs(grid, i - 1, j);
+        dfs(grid, i, j + 1);
+        dfs(grid, i, j - 1);
+    }
+}
---
+++ src/main/java/au/com/rainmore/graphs/No695MaxAreaOfIsland.java
@@ -0,0 +1,13 @@
+package au.com.rainmore.graphs;
+
+
+/**
+ * <a href="https://leetcode.com/problems/max-area-of-island/description/">
+ * 695. Max Area of Island</a>
+ */
+public class No695MaxAreaOfIsland {
+
+    public int maxAreaOfIsland(int[][] grid) {
+
+    }
+}
