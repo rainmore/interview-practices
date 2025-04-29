0c43af9bcba93bc2c5a16f49ed1ae834d9d2aa52
Felix Rong <felix@wytn.com.au>
2025-04-29 23:13:47 +1000

    upgraded gradle

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,6 +2,7 @@ rootProject.name = "hacker-rank"
 
 pluginManagement {
     plugins {
+        id("org.gradle.toolchains.foojay-resolver-convention").version(settings.extra["plugin.foojay.version"]!!.toString())
     }
 
     repositories {
